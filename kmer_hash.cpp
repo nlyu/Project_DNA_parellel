0a2841fda1afada6a733a3ca4fece4e5cb000a8a
nlyu <lnc0519@gmail.com>
2019-03-30 17:03:38 -0500

    1

--- kmer_hash.cpp
+++ kmer_hash.cpp
@@ -5,8 +5,8 @@
 #include <set>
 #include <numeric>
 #include <cstddef>
-#include <chrono>
 #include <upcxx/upcxx.hpp>
+#include <chrono>
 
 #include "kmer_t.hpp"
 #include "read_kmers.hpp"
@@ -19,10 +19,10 @@ int main(int argc, char **argv) {
 
   // TODO: remove this, when you start writing
   // parallel implementation.
-  if (upcxx::rank_n() > 1) {
-    throw std::runtime_error("Error: parallel implementation not started yet!"
-      " (remove this when you start working.)");
-  }
+  // if (upcxx::rank_n() > 1) {
+  //   throw std::runtime_error("Error: parallel implementation not started yet!"
+  //     " (remove this when you start working.)");
+  // }
 
   if (argc < 2) {
     BUtil::print("usage: srun -N nodes -n ranks ./kmer_hash kmer_file [verbose|test]\n");
@@ -44,12 +44,19 @@ int main(int argc, char **argv) {
       std::to_string(ks) + "-mers, while this binary is compiled for " +
       std::to_string(KMER_LEN) + "-mers.  Modify packing.hpp and recompile.");
   }
-
+  // Total number of kmers from all ranks.
   size_t n_kmers = line_count(kmer_fname);
 
   // Load factor of 0.5
   size_t hash_table_size = n_kmers * (1.0 / 0.5);
+
+  size_t nprocs = upcxx::rank_n();
+  size_t my_rank = upcxx::rank_me();
+  // hash_table_size = (hash_table_size + nprocs - 1)/nprocs;
+
   HashMap hashmap(hash_table_size);
+
+  // Build global pointer for data and used.
 
   if (run_type == "verbose") {
     BUtil::print("Initializing hash table of size %d for %d kmers.\n",
@@ -62,6 +69,9 @@ int main(int argc, char **argv) {
     BUtil::print("Finished reading kmers.\n");
   }
 
+  //
+  // Building local hashmap and start nodes array.
+  //
   auto start = std::chrono::high_resolution_clock::now();
 
   std::vector <kmer_pair> start_nodes;
@@ -76,17 +86,45 @@ int main(int argc, char **argv) {
       start_nodes.push_back(kmer);
     }
   }
-  auto end_insert = std::chrono::high_resolution_clock::now();
-  upcxx::barrier();
+  if (run_type == "verbose"){
+    upcxx::barrier();
+    int *lp = hashmap.used[upcxx::rank_me()].local();
+    int count =0;
+    for (int i=0;i<hashmap.my_size; i++){
+      if(*(lp + i) != lp[i]) printf("errordsfadfasd\n");
+      count+= lp[i]>0;
+    }
+    kmer_pair * kp = hashmap.data[upcxx::rank_me()].local();
+    printf("my size: %d\n", hashmap.my_size);
+    upcxx::barrier();
+    printf(" Wrote %d in rank %d\n", count, upcxx::rank_me());
+    upcxx::barrier();
+    printf("First 3 elements in rank %d: %s = %d, %s = %d, %s = %d\n", upcxx::rank_me(), kp[0].kmer_str().c_str(), lp[0], kp[1].kmer_str().c_str(), lp[1],  kp[2].kmer_str().c_str(), lp[2]);
 
+    // Write to file
+    std::ofstream fout("verbose_" + std::to_string(upcxx::rank_me()) + ".dat");
+    for (int i=0; i<hashmap.my_size; i++) {
+      fout << kp[i].kmer_str().c_str() << ' ' << std::to_string(lp[i]) << std::endl;
+    }
+    fout.close();
+  }
+  auto end_insert = std::chrono::high_resolution_clock::now();
+
+  // Build global pointer for used.
+
+
+  // Measure time of buildinghash table.
+  upcxx::barrier();
   double insert_time = std::chrono::duration <double> (end_insert - start).count();
   if (run_type != "test") {
     BUtil::print("Finished inserting in %lf\n", insert_time);
   }
   upcxx::barrier();
 
+  //
+  // building contigs
+  //
   auto start_read = std::chrono::high_resolution_clock::now();
-
   std::list <std::list <kmer_pair>> contigs;
   for (const auto &start_kmer : start_nodes) {
     std::list <kmer_pair> contig;
@@ -95,6 +133,7 @@ int main(int argc, char **argv) {
       kmer_pair kmer;
       bool success = hashmap.find(contig.back().next_kmer(), kmer);
       if (!success) {
+        printf("Doesn't find kmer str %s, end at kmer str %s \n", contig.back().next_kmer().get().c_str(), kmer.kmer_str().c_str());
         throw std::runtime_error("Error: k-mer not found in hashmap.");
       }
       contig.push_back(kmer);
