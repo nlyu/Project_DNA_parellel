24bd7e11787dae272fb2e3e0f7b1df158cbd9187
nlyu <lnc0519@gmail.com>
2019-03-30 20:41:14 -0500

    4

--- mybenmark.sh
+++ mybenmark.sh
@@ -1,11 +1,11 @@
 echo '19-kmer smaller file test result'
 
-dna_path='./kmer_hash /global/project/projectdirs/mp309/cs267-spr2018/hw3-datasets/'
-core=('1', '2', '4', '8', '16', '32')
+dna_path=./kmer_hash /global/project/projectdirs/mp309/cs267-spr2018/hw3-datasets/
+core=(1, 2, 4, 8, 16, 32)
 
-file='smaller/small.txt'
+file=smaller/small.txt
 for value in "${core[@]}"
 do
-        echo 'n = ' ${value}
-        srun -N 1 -n ${value} ${dna_path}${file}
+        echo 'n = ' $value
+        srun -N 1 -n $value $dna_path$file
 done
