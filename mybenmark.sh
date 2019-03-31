8046625b42f83bf62869641e7ccf26846e16684b
nlyu <lnc0519@gmail.com>
2019-03-30 20:34:34 -0500

    4

--- mybenmark.sh
+++ mybenmark.sh
@@ -1,7 +1,7 @@
 echo '19-kmer smaller file test result'
 
-dna_path = './kmer_hash /global/project/projectdirs/mp309/cs267-spr2018/hw3-datasets/'
-core = (1, 2, 4, 8, 16, 32)
+dna_path='./kmer_hash /global/project/projectdirs/mp309/cs267-spr2018/hw3-datasets/'
+core=(1, 2, 4, 8, 16, 32)
 
 file = 'smaller/small.txt'
 for value in "${core[@]}"
